d5e254cfa5627c58794cc30bd1f5289429a525b2
yeonseong <yeonseong.dev@gmail.com>
2023-02-23 02:40:25 +0900

    FEAT: drewPixel

---
+++ src/Component/Render/_drawPixel.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+	// TODO: refactory for performance,
+	// @see https://dirask.com/posts/JavaScript-how-to-draw-pixel-on-canvas-element-n1e7Wp
+	export const drawPixel = (ctx, x, y, color) => {
+		const roundedX = Math.round(x);
+		const roundedY = Math.round(y);
+		ctx.fillStyle = color || rgba(0, 0, 0, 0);
+		ctx.fillRect(roundedX, roundedY, 1, 1);
+	};
+</script>
--- src/Component/Render/_rendering.svelte
+++ src/Component/Render/_rendering.svelte
@@ -1,10 +1,21 @@
 <script context="module">
+	import { drawPixel } from "./_drawPixel.svelte";
+
 	export const rendering = () => {
 		const canvas = document.getElementById(process.env.CANVAS_NAME);
-		if (!canvas?.getContext) {
+		if (!canvas.getContext) {
 			alert("use other broswer");
 			return;
 		}
 		const ctx = canvas.getContext("2d");
+		var colors = ["red", "blue", "orange", "yellow", "brown", "green"];
+
+		for (var i = 0; i < 10000; ++i) {
+			var x = canvas.width * Math.random();
+			var y = canvas.height * Math.random();
+			var color = colors[i % colors.length];
+			console.log(i);
+			drawPixel(ctx, x, y, color);
+		}
 	};
 </script>
