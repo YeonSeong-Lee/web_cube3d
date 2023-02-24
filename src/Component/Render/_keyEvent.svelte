725ee784a0948f09c6b40d6f8c56da765a9679d9
yeonseong <yeonseong.dev@gmail.com>
2023-02-25 02:55:14 +0900

    WIP : keyEvent

---
+++ src/Component/Render/_keyEvent.svelte
@@ -0,0 +1,41 @@
+<script context="module">
+	export class KeyHandler {
+		constructor() {
+			this.right = false;
+			this.left = false;
+			this.up = false;
+			this.down = false;
+			this.jump = false;
+			this.keyDownHandler = this.keyDownHandler.bind(this);
+			this.keyUpHandler = this.keyUpHandler.bind(this);
+		}
+
+		keyDownHandler(e) {
+			if (e.key == 37 || e.key == "ArrowRight") {
+				this.right = true;
+			} else if (e.key == 39 || e.key == "ArrowLeft") {
+				this.left = true;
+			} else if (e.key == 38 || e.key == "ArrowUp") {
+				this.up = true;
+			} else if (e.key == 40 || e.key == "ArrowDown") {
+				this.down = true;
+			} else if (e.key == " " || e.code == "Space") {
+				this.jump = true;
+			}
+		}
+
+		keyUpHandler(e) {
+			if (e.key == 37 || e.key == "ArrowRight") {
+				this.right = false;
+			} else if (e.key == 39 || e.key == "ArrowLeft") {
+				this.left = false;
+			} else if (e.key == 38 || e.key == "ArrowUp") {
+				this.up = false;
+			} else if (e.key == 40 || e.key == "ArrowDown") {
+				this.down = false;
+			} else if (e.key == " " || e.code == "Space") {
+				this.jump = false;
+			}
+		}
+	}
+</script>
--- src/Component/Render/_rendering.svelte
+++ src/Component/Render/_rendering.svelte
@@ -1,21 +1,37 @@
 <script context="module">
-	import { drawPixel } from "./_drawPixel.svelte";
+	import { drawRect } from "./_draw.svelte";
+	import { KeyHandler } from "./_keyEvent.svelte";
 
 	export const rendering = () => {
+		setInterval(draw, 1000);
+	};
+
+	export const draw = () => {
+		const keyHandler = new KeyHandler();
 		const canvas = document.getElementById(process.env.CANVAS_NAME);
 		if (!canvas.getContext) {
-			alert("use other broswer");
+			alert("use other browser");
 			return;
 		}
 		const ctx = canvas.getContext("2d");
-		var colors = ["red", "blue", "orange", "yellow", "brown", "green"];
-
-		for (var i = 0; i < 10000; ++i) {
-			var x = canvas.width * Math.random();
-			var y = canvas.height * Math.random();
-			var color = colors[i % colors.length];
-			console.log(i);
-			drawPixel(ctx, x, y, color);
+		let x = 0;
+		let y = 0;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		const step = 42;
+		if (keyHandler.right === true) {
+			x += step;
 		}
+		if (keyHandler.left === true) {
+			x -= step;
+		}
+		if (keyHandler.up === true) {
+			y += step;
+		}
+		if (keyHandler.down === true) {
+			y -= step;
+		}
+		drawRect(ctx, x, y, "red");
+		document.addEventListener("keyup", keyHandler.keyUpHandler, false);
+		document.addEventListener("keydown", keyHandler.keyDownHandler, false);
 	};
 </script>
