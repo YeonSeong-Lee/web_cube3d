18a798efa273075ea8b8a038972d66af1847bc88
yeonseong <yeonseong.dev@gmail.com>
2023-02-27 04:43:20 +0900

    FEAT: getWallColor

---
+++ src/Component/Render/_getWallColor.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export const getWallColor = (element) => {
+		switch (element) {
+			case 1:
+				return "blue";
+			case 2:
+				return "green";
+			case 3:
+				return "black";
+			case 4:
+				return "yellow";
+			default:
+				return "white";
+		}
+	};
+</script>
